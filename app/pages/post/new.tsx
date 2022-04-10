0367592ffb1931073bb82b8e51fe1857a9152ed7
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2022-04-09 21:48:17 -0400

    Update new.tsx

--- app/pages/post/new.tsx
+++ app/pages/post/new.tsx
@@ -2,29 +2,37 @@ import { DogProfile } from "@prisma/client"
 import PostForm from "app/core/components/postForm"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
 import Layout from "app/core/layouts/Layout"
+import getDogProfiles from "app/dog-profiles/queries/getDogProfiles"
 import createPost from "app/posts/mutations/createPost"
-import { useMutation, useRouter } from "blitz"
+import { useMutation, useQuery, useRouter } from "blitz"
+import db from "db"
 import { Formik, Form, Field, ErrorMessage } from "formik"
 import { BlitzPage } from "next"
-import React from "react"
+import React, { useState } from "react"
 
 interface FormValues {
   location: string
   body: string
+  pet: string
 }
 
 interface FormErrors {
-  locationError: string
+  errorMessage: string
 }
 
 const NewPost: BlitzPage = () => {
   const user = useCurrentUser()
   const router = useRouter()
   const [createAPost] = useMutation(createPost)
+  const [pet, setPet] = useState<string>("")
+  ///////////////////////////////////
   const initValues: FormValues = {
     location: "",
     body: "",
+    pet: "",
   }
+  ////////////////////////////////////
+  const [{ dogProfiles }] = useQuery(getDogProfiles, { where: { user_id: user!.id } })
 
   return (
     <React.Fragment>
@@ -32,18 +40,30 @@ const NewPost: BlitzPage = () => {
       {/* <PostForm props={user} /> */}
       <Formik
         initialValues={initValues}
+        validate={(values) => {
+          const errors: any = {}
+          if (values.pet === " " || values.pet === "") {
+            errors.errorMessage = "You must select a pet"
+          }
+          if (values.pet !== " ") {
+            console.log(values.pet)
+            setPet(values.pet)
+          }
+          return errors
+        }}
         onSubmit={async (values, actions) => {
           console.log({ values })
 
           try {
-            const res = await createAPost({
-              created_by: user!.id,
-              is_disabled: false,
-              ...values,
-            })
-            if (res) {
-              alert("Post made")
-            }
+            // const res = await createAPost({
+            //   created_by: user!.id,
+            //   is_disabled: false,
+            //   ...values,
+            // })
+            // if (res) {
+            //   alert("Post made")
+            //   router.push("/home")
+            // }
           } catch (error) {
             console.log(error.message)
           }
@@ -52,10 +72,23 @@ const NewPost: BlitzPage = () => {
         }}
       >
         <Form>
-          <label htmlFor="location">Location</label>
+          <label htmlFor="location">Where are you taking</label>
+          <Field as="select" id="pet" name="pet">
+            <option value={" "} label={" "}>
+              {" "}
+            </option>
+            {dogProfiles.map((pets, idx) => (
+              <React.Fragment key={idx}>
+                <option value={pets.pet_name} label={pets.pet_name}>
+                  {pets.pet_name}
+                </option>
+              </React.Fragment>
+            ))}
+          </Field>
+
           <Field id="location" name="location" placeholder="Location" />
           <label htmlFor="body">Body</label>
-          <Field id="body" name="body" placeholder="Body" />
+          <Field id="body" name="body" placeholder={`Where are you taking ${pet}?`} />
 
           <button type="submit">Submit</button>
         </Form>
