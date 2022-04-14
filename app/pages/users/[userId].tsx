3a52feacb4d9185f60523ee26bba4f3a7f248161
Emanuel Tesfa <emanueltesfa0@gmail.com>
2022-04-14 14:42:38 -0400

    Merge branch 'main' of https://github.com/emanueltesfa/HOWL into AmansBranch

--- app/pages/users/[userId].tsx
+++ app/pages/users/[userId].tsx
@@ -4,27 +4,52 @@ import Layout from "app/core/layouts/Layout"
 import getUser from "app/users/queries/getUser"
 import deleteUser from "app/users/mutations/deleteUser"
 import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+import getPosts from "app/posts/queries/getPosts"
+import ScrollPost from "app/core/components/scrollPost"
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth"
+import getDogProfileUserId from "app/dog-profiles/queries/getDogProfileUserId"
 
 export const User = () => {
   const router = useRouter()
   const userId = useParam("userId", "number")
   const [deleteUserMutation] = useMutation(deleteUser)
   const [user] = useQuery(getUser, { id: userId })
-
   const currentUser = useCurrentUser()
-  console.log(user.id)
+  const [{ posts }] = useQuery(getPosts, { where: { created_by: user.id } })
+  const [dogProfile] = useQuery(getDogProfileUserId, { user_id: user.id })
+
+  console.log(user)
   console.log(currentUser)
+  console.log()
+
+  // Check if user is the same user that is logged in or visitor
+  // {flag ? <MYprofile /> :<otherprofile />}
   return (
     <>
-      <Head>
-        <title>User {user.id}</title>
-      </Head>
-
       <div>
+        <div className="HumanProf">
+          <h1>Welcome {user.name}</h1>
+          <button
+            onClick={() => {
+              router.push("/post/new")
+            }}
+          >
+            Make Post!
+          </button>
+          <CalendarMonthIcon />
+        </div>
+
+        <ScrollPost />
+        <div className="DogProf">
+          <h1>Hey {dogProfile.pet_name}!</h1>
+        </div>
+
+        <Head>
+          <h1>Name {user.name}</h1>
+        </Head>
+
         <h1>User {user.id}</h1>
         <pre>{JSON.stringify(user, null, 2)}</pre>
-        <div>Hellofdsafjdsafjdsfa</div>
-        <div> </div>
 
         {/* <Link href={Routes.EditUserPage({ userId: user.id })}>
           <a>Edit</a>
@@ -50,11 +75,13 @@ export const User = () => {
 const ShowUserPage: BlitzPage = () => {
   return (
     <div>
-      {/* <p>
-        <Link href={Routes.UsersPage()}>
-          <a>Users</a>
-        </Link>
-      </p> */}
+      {
+        <p>
+          <Link href={Routes.UsersPage()}>
+            <a>Users</a>
+          </Link>
+        </p>
+      }
 
       <Suspense fallback={<div>Loading...</div>}>
         <User />
