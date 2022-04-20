f2d8dd32c110a789aa30dc72a41fe5ba039bb24b
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2022-04-19 22:31:33 -0400

    Creating Functionality of profile edit form

--- app/pages/users/[userId]/edit.tsx
+++ app/pages/users/[userId]/edit.tsx
@@ -1,9 +1,28 @@
-import { Suspense } from "react"
+import React, { Suspense, useState } from "react"
 import { Head, Link, useRouter, useQuery, useMutation, useParam, BlitzPage, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getUser from "app/users/queries/getUser"
 import updateUser from "app/users/mutations/updateUser"
 import { UserForm, FORM_ERROR } from "app/users/components/UserForm"
+import * as Yup from "yup"
+import { Field, Form, Formik } from "formik"
+import { createStore } from "state-pool"
+
+const store = createStore()
+
+store.setState("userName", "")
+
+interface UserData {
+  name: string
+  profile_pic: string
+  dob: string
+}
+
+const FormSchema = Yup.object().shape({
+  name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required!"),
+  profile_pic: Yup.string(),
+  dob: Yup.string().required("Required!"),
+})
 
 export const EditUser = () => {
   const router = useRouter()
@@ -17,40 +36,84 @@ export const EditUser = () => {
     }
   )
   const [updateUserMutation] = useMutation(updateUser)
+  const [userName, setUserName] = store.useState("userName")
+  const [timeoutFlag, setTimeoutFlag] = useState<boolean>(false)
+  setUserName(user.name)
+
+  const initValues: UserData = {
+    name: user!.name!,
+    profile_pic: "",
+    dob: user.dob,
+  }
 
   return (
     <>
       <Head>
-        <title>Edit User {user.id}</title>
+        <title>Edit User {userName}</title>
       </Head>
 
       <div>
-        <h1>Edit User {user.id}</h1>
+        <h1>Edit User {userName}</h1>
         <pre>{JSON.stringify(user, null, 2)}</pre>
-
-        <UserForm
-          submitText="Update User"
-          // TODO use a zod schema for form validation
-          //  - Tip: extract mutation's schema into a shared `validations.ts` file and
-          //         then import and use it here
-          // schema={UpdateUser}
-          initialValues={user}
+        <Formik
+          initialValues={initValues}
+          validationSchema={FormSchema}
           onSubmit={async (values) => {
+            setTimeoutFlag(true)
+            setTimeout(() => {
+              setTimeoutFlag(false)
+            }, 1000)
+            console.log("Submitting")
+            const { profile_pic } = values
+            let profile_picFile: string
+            const profile_pic_split: string[] | undefined = profile_pic.split("\\")
+            profile_picFile = profile_pic_split[profile_pic_split.length - 1]!
+            profile_picFile = ""
+            console.log({ values })
             try {
-              const updated = await updateUserMutation({
+              const res = await updateUserMutation({
                 id: user.id,
+                profile_pic_file: profile_picFile,
                 ...values,
               })
-              await setQueryData(updated)
-              router.push(Routes.ShowUserPage({ userId: updated.id }))
-            } catch (error: any) {
-              console.error(error)
-              return {
-                [FORM_ERROR]: error.toString(),
+              if (res) {
+                console.log(res)
+                setQueryData(res)
               }
+            } catch (error) {
+              console.log(error.message)
             }
           }}
-        />
+        >
+          {({ errors, touched, isValidating, isSubmitting }) => (
+            <Form>
+              <Field name="name" disabled={timeoutFlag}>
+                {({
+                  field, // { name, value, onChange, onBlur }
+                  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                  meta,
+                }) => (
+                  <div>
+                    <input
+                      type="text"
+                      placeholder={`${userName}`}
+                      value={userName}
+                      onChange={(e) => setUserName(e.target.value)}
+                      disabled={timeoutFlag}
+                      {...field}
+                    />
+                    {meta.touched && meta.error && <div className="error">{meta.error}</div>}
+                  </div>
+                )}
+              </Field>
+              <Field type="file" name="profile_pic" disabled={timeoutFlag} />
+              <Field type="date" name="dob" disabled={timeoutFlag} />
+              <button type="submit" disabled={timeoutFlag}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </>
   )
@@ -62,12 +125,6 @@ const EditUserPage: BlitzPage = () => {
       <Suspense fallback={<div>Loading...</div>}>
         <EditUser />
       </Suspense>
-
-      <p>
-        <Link href={Routes.UsersPage()}>
-          <a>Users</a>
-        </Link>
-      </p>
     </div>
   )
 }
