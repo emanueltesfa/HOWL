bbbfd691fa9f3495de04619bdd6ce4fe12efd2f5
ambrown0928 <ambrown0928@gmail.com>
2022-04-28 11:30:22 -0400

    Cleaning up the code

--- app/api/upload_image.ts
+++ app/api/upload_image.ts
@@ -3,9 +3,6 @@ import * as CryptoJS from "crypto-js"
 import image from "next/image"
 
 function upload_image(name: string, filename: string, data: File) {
-  const AZURE_STORAGE_CONNECTION_STRING =
-    "DefaultEndpointsProtocol=https;AccountName=howlimagestorage;AccountKey=RPooP/NLpuH1Yr3d07/uhyYLkxE18hFucXKXVsMeg8bN5RciEEKhcELT26I/E9+vQXpQcHPOoA0I+ASt5Nse/g==;EndpointSuffix=core.windows.net"
-
   const accountName = "howlimagestorage"
   const key =
     "RPooP/NLpuH1Yr3d07/uhyYLkxE18hFucXKXVsMeg8bN5RciEEKhcELT26I/E9+vQXpQcHPOoA0I+ASt5Nse/g=="
@@ -17,7 +14,6 @@ function upload_image(name: string, filename: string, data: File) {
   const signedexpiry = end.toISOString().substring(0, end.toISOString().lastIndexOf(".")) + "Z"
   const signedProtocol = "https"
   const signedversion = "2018-03-28"
-
   const StringToSign =
     accountName +
     "\n" +
@@ -35,16 +31,12 @@ function upload_image(name: string, filename: string, data: File) {
     "\n" +
     signedversion +
     "\n"
-  console.log("hi")
   var str = CryptoJS.HmacSHA256(StringToSign, CryptoJS.enc.Base64.parse(key))
   var sig = CryptoJS.enc.Base64.stringify(str)
   const sasToken = `sv=${signedversion}&ss=${signedservice}&srt=${signedresourcetype}&sp=${signedpermissions}&se=${encodeURIComponent(
     signedexpiry
   )}&spr=${signedProtocol}&sig=${encodeURIComponent(sig)}`
 
-  if (!AZURE_STORAGE_CONNECTION_STRING) {
-    throw Error("Azure Storage Connection string not found")
-  }
   const pipeline = newPipeline(new AnonymousCredential())
 
   const service_client = new BlobServiceClient(
