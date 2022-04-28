695ae8241c9e67ce8cb2542f1745f24b404208da
ambrown0928 <ambrown0928@gmail.com>
2022-04-28 11:20:07 -0400

    working image upload code
    Image upload is now working. All it needs is an upload to the database and it'll be working perfectly

--- app/core/components/select_image/SelectImage.tsx
+++ app/core/components/select_image/SelectImage.tsx
@@ -1,8 +1,10 @@
 import upload_image from "app/api/upload_image"
+import { Image } from "blitz"
 
 const SelectImage = ({ container_name }) => {
   var selected_file
-
+  var url: string =
+    "https://howlimagestorage.blob.core.windows.net/profile1/default-profile-icon-24.jpg"
   const OnFileChange = (event: any) => {
     selected_file = event.target.files[0]
     console.log(selected_file.name)
@@ -12,15 +14,16 @@ const SelectImage = ({ container_name }) => {
     const form_data = new FormData()
     const filename = selected_file.name
 
-    upload_image(container_name, filename, selected_file)
-
-    console.log(selected_file)
+    url = upload_image(container_name, filename, selected_file)
+    
+    console.log(url)
   }
 
   return (
     <div>
       <input type="file" onChange={OnFileChange} />
       <button onClick={OnFileUpload}>submit</button>
+      <Image src={url} alt="e" height="100px" width="100px" />
     </div>
   )
 }
--- blitz.config.ts
+++ blitz.config.ts
@@ -7,7 +7,10 @@ const config: BlitzConfig = {
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
-  
+  images: {
+    domains: ["howlimagestorage.blob.core.windows.net"],
+  },
+
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
